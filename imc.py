b5354ef7e88875bfb4b4dea9094cea1f1fbdc13c
Viarex <72530642+Viarex@users.noreply.github.com>
2020-10-07 22:15:34 -0300

    Upload do meu IMC

---
+++ imc.py
@@ -0,0 +1,27 @@
+def numero_quadrado(numero):
+  quadrado = numero * numero
+  return quadrado
+
+def calcular_imc(peso, altura):
+  altura_quadrada = numero_quadrado(altura)
+  meu_imc = peso / altura_quadrada
+  return meu_imc
+
+def classificar_imc(meu_imc):
+  if imc < 18.5:
+    print('Magreza')
+  elif imc >= 18.5 and imc < 25:
+    print('Normal')
+  elif imc >= 25 and imc < 30:
+    print('Sobrepeso')
+  elif imc >= 30 and imc < 40:
+    print('Obesidade')
+  else:
+    print('Obesidade Grave')
+
+imc = calcular_imc(110, 1.76)
+
+print('Seu IMC é:', imc)
+
+print('Sua classificação é:')
+classificar_imc(imc)
